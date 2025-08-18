c0fc4fd088f525d6486b285b7c5beb0f35266415
Gunter M Gaede <guntermg@hotmail.com>
2025-08-17 23:09:14 -0300

    initializing git repo

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  standalone: false,
+})
+export class AppComponent implements OnInit {
+
+  public employees: Employee[] = [];
+
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit() {
+      this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { EmployeeService } from "./employee.service";
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports:
+    [
+      BrowserModule
+    ],
+  providers: [EmployeeService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
